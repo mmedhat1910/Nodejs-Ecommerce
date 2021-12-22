7ed90bed6d417884e8b3fd2792be26c64446d3c4
Mohamed Medhat <mmedhat1910@gmail.com>
2021-12-22 18:23:44 +0200

    Done with register [Cookie problem]

--- models/users.js
+++ models/users.js
@@ -10,20 +10,8 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: false,
-        default: null
-    },
-    phoneNumber: {
-        type: String,
-        required: false,
-        default: null
-    }
+
+
 });
 
 const UserModel = model('user', UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
-const UserModel = require('./../models/users')
+const UserModel = require('./../models/users');
+const bcrypt = require('bcrypt');
 const router = Router();
 
 router.get('/login', (req, res) => {
@@ -9,21 +10,39 @@ router.get('/login', (req, res) => {
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     //todo
-    const user = await UserModel.findOne({ username: username })
-
+    const user = await UserModel.findOne({ username: username });
+    const isPasswordCorrect = await bcrypt.compare(user.password, password);
     if (!user) {
         return res.render('login.ejs', { status: 404 });
     }
-    else if (password !== user.password) {
+    else if (isPasswordCorrect) {
         return res.render('login.ejs', { status: 401 });
     }
-    res.setHeader('Set-Cookie', 'loggedin=true')
+    //TODO: Cookie not working
+    res.setHeader('Set-Cookie', `loggedin=true;username=${username}`);
     return res.redirect('/');
 
 })
 
 router.get('/register', (req, res) => {
-    res.render('registration');
+    res.render('registration.ejs', { status: 200 });
+})
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username: username })
+    if (user) {
+        console.log(user)
+        return res.render('registration.ejs', { status: 409 });
+    } else {
+        try {
+            const hashPassword = await bcrypt.hash(password, 10);
+            await UserModel.create({ username: username, password: hashPassword });
+            res.setHeader('Set-Cookie', `loggedin=true;username=${username}`);
+            res.redirect('/');
+        } catch (e) {
+            return res.render('registration.ejs', { status: 400 });
+        }
+    }
 })
 
-module.exports = router
+module.exports = router;
