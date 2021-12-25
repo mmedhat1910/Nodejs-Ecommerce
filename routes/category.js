3b4fac0c99541e8c96ede8339e90bdc4798248e2
Mohamed Medhat <mmedhat1910@gmail.com>
2021-12-25 21:09:58 +0200

    Re-created files

--- routes/category.js
+++ routes/category.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const router = Router();
-const itemModel = require('./../models/item')
+const itemModel = require('./../models/item');
 
 router.get('/:category', async (req, res) => {
     const category = req.params.category;
     try {
         const itemList = await itemModel.find({ category: category });
         if (itemList && itemList.length > 0) {
-            return res.render('phones.ejs', { itemList: itemList });
+            return res.render('category.ejs', { itemList: itemList });
         }
     } catch (err) {
         console.error(err);
